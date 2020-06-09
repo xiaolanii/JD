420a9eb6a157adf4b10ab1ea126d9f43fd056a92
xiaolani <840824798@qq.com>
2020-06-09 10:49:10 +0800

    对good页面进行样式修改，补回不知道为什么丢失放大镜的js代码

--- README.md
+++ README.md
@@ -6,3 +6,31 @@
 
 功能：
 注册登录、展示商品、轮播图、三级联动表单、购物车、管理员等模块
+
+
+
+# JD项目
+
+## 使用NodeJS完成本次的代码
+路由分配规则为restful
+
+本次项目是为了完成作业
+
+功能：
+注册登录、展示商品、轮播图、三级联动表单、购物车、管理员等模块
+
+## 使用本项目时，新建数据库表，表名如图
+
+good表
+
+![image-20200608121557594](C:\Users\84082\AppData\Roaming\Typora\typora-user-images\image-20200608121557594.png)
+
+orders表
+
+![image-20200608121615314](C:\Users\84082\AppData\Roaming\Typora\typora-user-images\image-20200608121615314.png)
+
+user表
+
+![image-20200608121636043](C:\Users\84082\AppData\Roaming\Typora\typora-user-images\image-20200608121636043.png)
+
+主键都为自增长
--- public/js/good.js
+++ public/js/good.js
@@ -33,4 +33,61 @@ $(function () {
         })
     })
 
+    var box = my$('box');
+    var smallBox = box.children[0];
+    var bigBox = box.children[1];
+
+    var smallImage = smallBox.children[0];
+    var mask = smallBox.children[1];
+    var bigImage = bigBox.children[0];
+
+    box.onmouseenter = function () {
+        // 显示 mask和bigBox
+        mask.style.display = 'block';
+        bigBox.style.display = 'block'
+    }
+
+    box.onmouseleave = function () {
+        mask.style.display = 'none';
+        bigBox.style.display = 'none';
+    }
+
+    // 2 当鼠标在盒子中移动的时候，让mask和鼠标一起移动
+    box.onmousemove = function (e) {
+        e = e || window.event;
+        // 获取鼠标在盒子中的位置，就是mask的坐标
+        var maskX = getPage(e).pageX - box.offsetLeft;
+        var maskY = getPage(e).pageY - box.offsetTop;
+
+        // 让鼠标出现在mask的中心点
+        maskX = maskX - mask.offsetWidth / 2;
+        maskY = maskY - mask.offsetHeight / 2;
+
+        // 把mask限制到box中
+        maskX = maskX < 0 ? 0 : maskX;
+        maskY = maskY < 0 ? 0 : maskY;
+
+        maskX = maskX > box.offsetWidth - mask.offsetWidth ? box.offsetWidth - mask.offsetWidth : maskX;
+        maskY = maskY > box.offsetHeight - mask.offsetHeight ? box.offsetHeight - mask.offsetHeight : maskY;
+
+
+        mask.style.left = maskX + 'px';
+        mask.style.top = maskY + 'px';
+        // 3 当mask移动的时候，让大图片移动
+        // 求 大图片移动的距离
+
+        // mask移动的距离 / mask最大能够移动的距离  = 大图片移动的距离 / 大图片最大能够移动的距离
+
+        // mask最大能够移动的距离
+        var maskMax = box.offsetWidth - mask.offsetWidth;
+        // 大图片最大能够移动的距离
+        var bigImageMax = bigImage.offsetWidth - bigBox.offsetWidth;
+
+        var bigImageX = maskX * bigImageMax / maskMax;
+        var bigImageY = maskY * bigImageMax / maskMax;
+
+        bigImage.style.left = -bigImageX + 'px';
+        bigImage.style.top = -bigImageY + 'px';
+    }
+
 });
